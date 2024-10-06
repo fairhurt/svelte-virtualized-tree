0eaf09bf8d57decde84c4d034f66262860769228
Tyson Fairhurst <55992872+fairhurt@users.noreply.github.com>
2024-10-06 14:07:32 -0700

    Add Stackblitz

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -17,7 +17,7 @@
 	</h2>
 	<div class="m-4">
 		<code class="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-			npm i svelte-virtual-tree
+			npm i svelte-virtualized-tree
 		</code>
 	</div>
 </div>
--- src/routes/usage/+page.svelte
+++ src/routes/usage/+page.svelte
@@ -1,1 +1,9 @@
-<div>usage</div>
+<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">Example Usage</h3>
+
+<a
+	href="https://stackblitz.com/github/fairhurt/svelte-virtualized-tree-example"
+	target="_blank"
+	rel="noopener noreferrer"
+>
+	<img alt="Open in StackBlitz" src="https://developer.stackblitz.com/img/open_in_stackblitz.svg" />
+</a>
