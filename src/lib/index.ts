91cd5eadf429317760835b4ac76975d9b2b967e5
Tyson Fairhurst <tysonfairhurst@gmail.com>
2024-10-19 14:57:44 -0700

    Initial headless  (#1)
    * 1.0.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,68 +1,67 @@
-import VirtualizedTree from "./virtualized-tree.svelte";
-import type { TreeItem, VirtualizedTreeOptions, TreeItemAccessorKey, VirtualizedTreeIcons } from "./types";
-/**
- * Represents a virtualized tree structure for efficiently rendering large tree data.
- *
- * @template T - The type of the tree item.
- */
-class VirtualizedTreeInstance<T> {
-    data: TreeItem<T>[];
-    options?: VirtualizedTreeOptions<T>;
-    accessorKey: TreeItemAccessorKey<T>;
-    icons?: VirtualizedTreeIcons;
-    selectedId?: string | number ;
-    visibleNodes: TreeItem<T>[] = [];
-    constructor({ data, accessorKey, options, icons }: { data: TreeItem<T>[]; accessorKey: TreeItemAccessorKey<T>; icons?: VirtualizedTreeIcons; options?: VirtualizedTreeOptions<T> }) {
-        this.data = data;
-        this.options = options;
-        this.accessorKey = accessorKey;
-        this.icons = icons;
-        this.selectedId = undefined; // Initialize selectedId to undefined
-    }
+import { TreeVirtualizer, observeElementRect, type VirtualizerOptions } from './core';
+import { derived, writable } from 'svelte/store';
+import type { PartialKeys } from './core/utils';
+import type { Readable, Writable } from 'svelte/store';
 
-    /**
-     * Retrieves the display value of a tree item at the specified index.
-     *
-     * @template T - The type of the tree item.
-     * @param {number} index - The index of the tree item in the data array.
-     * @returns {TreeItem<T>[keyof TreeItem<T>]} - The display value of the tree item.
-     */
-    getDisplayValue(index: number): T[keyof T] {
-        return this.data[index][this.accessorKey] as T[keyof T];
-    }
+export type SvelteTreeVirtualizer<
+	TScrollElement extends Element | Window,
+	TItemElement extends Element,
+	TData
+> = Omit<TreeVirtualizer<TScrollElement, TItemElement, TData>, 'setOptions'> & {
+	setOptions: (options: Partial<VirtualizerOptions<TScrollElement, TItemElement, TData>>) => void;
+};
 
-    /**
-     * Retrieves the currently selected ID.
-     *
-     * @returns {string | number | undefined} The ID of the selected item, or `undefined` if no item is selected.
-     */
-    getSelectedId(): string | number | undefined {
-        return this.selectedId;
-    }
+function createTreeVirtualizerBase<
+	TScrollElement extends Element | Window,
+	TItemElement extends Element,
+	TData
+>(
+	initialOptions: VirtualizerOptions<TScrollElement, TItemElement, TData>
+): Readable<SvelteTreeVirtualizer<TScrollElement, TItemElement, TData>> {
+	const virtualizer = new TreeVirtualizer(initialOptions);
+	const originalSetOptions = virtualizer.setOptions;
 
-    /**
-     * Sets the selected ID.
-     *
-     * @param id - The ID to be set as selected. Can be a string or a number.
-     */
-    setSelectedId(id: string | number): void {
-        this.selectedId = id;
-    }
-    
+	// eslint-disable-next-line prefer-const
+	let virtualizerWritable: Writable<TreeVirtualizer<TScrollElement, TItemElement, TData>>;
+
+	const setOptions = (
+		options: Partial<VirtualizerOptions<TScrollElement, TItemElement, TData>>
+	) => {
+		const resolvedOptions = {
+			...virtualizer.options,
+			...options,
+			onChange: options.onChange
+		};
+		originalSetOptions({
+			...resolvedOptions,
+			onChange: (instance: TreeVirtualizer<TScrollElement, TItemElement, TData>, sync: boolean) => {
+				virtualizerWritable.set(instance);
+				resolvedOptions.onChange?.(instance, sync);
+			}
+		});
+		virtualizer._willUpdate();
+	};
+
+	virtualizerWritable = writable(virtualizer, () => {
+		setOptions(initialOptions);
+		return virtualizer._didMount();
+	});
+
+	return derived(virtualizerWritable, (instance) => Object.assign(instance, { setOptions }));
 }
 
-/**
- * Creates a new instance of a virtualized tree.
- *
- * @template T - The type of the data items in the tree.
- * @param {TreeItem<T>[]} data - An array of tree items to be used as the data source.
- * @param {VirtualizedTreeOptions<T>} options - Configuration options for the virtualized tree.
- * @returns {VirtualizedTree<T>} A new instance of a virtualized tree.
- */
-function createVirtualizedTree<T>({ data, accessorKey, options, icons }: { data: TreeItem<T>[]; accessorKey: TreeItemAccessorKey<T>; icons?: VirtualizedTreeIcons; options?: VirtualizedTreeOptions<T> }): VirtualizedTreeInstance<T> {
-    return new VirtualizedTreeInstance({ data, accessorKey, icons, options });
+export function createTreeVirtualizer<
+	TScrollElement extends Element,
+	TItemElement extends Element,
+	TData
+>(
+	options: PartialKeys<
+		VirtualizerOptions<TScrollElement, TItemElement, TData>,
+		'observeElementRect'
+	>
+): Readable<SvelteTreeVirtualizer<TScrollElement, TItemElement, TData>> {
+	return createTreeVirtualizerBase<TScrollElement, TItemElement, TData>({
+		observeElementRect: observeElementRect,
+		...options
+	});
 }
-
-export { VirtualizedTree, VirtualizedTreeInstance, createVirtualizedTree,
-type TreeItem, type VirtualizedTreeOptions, type TreeItemAccessorKey, type VirtualizedTreeIcons 
- };
--- src/routes/examples/basic/+page.svelte
+++ src/routes/examples/basic/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
-	import { VirtualizedTree } from '$lib';
-	import type { TreeItem } from '$lib/types';
-	import { createVirtualizedTree } from '$lib';
+	import type { VirtualTreeItem } from '$lib/core';
+	import { createTreeVirtualizer } from '$lib';
 	import { Separator } from '$site/components/ui/separator/index.js';
+	import Plus from 'lucide-svelte/icons/plus';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
+	import Check from 'lucide-svelte/icons/check';
+	import Minus from 'lucide-svelte/icons/minus';
+	import { createVirtualizer } from '@tanstack/svelte-virtual';
 
+	const icons = {
+		plus: Plus,
+		'chevron-right': ChevronRight,
+		'chevron-down': ChevronDown,
+		check: Check,
+		minus: Minus
+	};
 	type CustomTreeItem = {
 		displayName: string;
 	};
@@ -12,18 +24,18 @@
 		level: number,
 		parentId: number | null,
 		count: number
-	): TreeItem<CustomTreeItem>[] {
-		const items: TreeItem<CustomTreeItem>[] = [];
+	): VirtualTreeItem<CustomTreeItem>[] {
+		const items: VirtualTreeItem<CustomTreeItem>[] = [];
 
 		for (let i = 0; i < count; i++) {
 			const id = Math.floor(Math.random() * 10000);
-			const item: TreeItem<CustomTreeItem> = {
+			const item: VirtualTreeItem<CustomTreeItem> = {
 				id,
 				displayName: `Node ${id}`,
 				icon: 'plus',
 				level,
 				parentId,
-				children: level < 4 ? generateTreeData(level + 1, id, Math.floor(Math.random() * 3)) : []
+				children: level < 4 ? generateTreeData(level + 1, id, Math.floor(Math.random() * 4)) : []
 			};
 			totalNodes++;
 			items.push(item);
@@ -31,12 +43,29 @@
 		return items;
 	}
 
-	const treeData: TreeItem<CustomTreeItem>[] = generateTreeData(0, null, 3000);
+	const treeData: VirtualTreeItem<CustomTreeItem>[] = generateTreeData(0, null, 2000);
 
-	const virtualizedTree = createVirtualizedTree<CustomTreeItem>({
+	let virtualListEl: HTMLDivElement;
+	let virtualTreeEl: HTMLDivElement;
+
+	$: virtualizedTree = createTreeVirtualizer<HTMLDivElement, HTMLDivElement, CustomTreeItem>({
+		getScrollElement: () => virtualTreeEl,
 		data: treeData,
 		accessorKey: 'displayName'
 	});
+
+	$: virtualizer = createVirtualizer({
+		count: 0,
+		estimateSize: () => 35,
+		getScrollElement: () => virtualListEl,
+		overscan: 5
+	});
+	$: {
+		// Prevents re-creating the virtualizer if the size changes.
+		$virtualizer.setOptions({
+			count: $virtualizedTree.getVirtualTreeItems().length
+		});
+	}
 </script>
 
 <h4 class=" scroll-m-20 text-xl font-semibold tracking-tight">
@@ -44,5 +73,49 @@
 </h4>
 <Separator class="mb-1" />
 <div class="h-[600px] overflow-scroll">
-	<VirtualizedTree {virtualizedTree} />
+	<div bind:this={virtualTreeEl}>
+		<div style="" bind:this={virtualListEl}>
+			<div style="position: relative; height: {$virtualizer.getTotalSize()}px; width: 100%;">
+				{#each $virtualizer.getVirtualItems() as row (row.index)}
+					{@const node = $virtualizedTree.visibleItems[row.index]}
+					{@const hasChildren = !!node.children?.length}
+					{@const id = node.id}
+					{@const icon = node.icon}
+					{@const isExpanded = $virtualizedTree.expandedNodes.has(id)}
+					{@const content = node[$virtualizedTree.options.accessorKey]}
+					{@const selectedId = $virtualizedTree.getSelectedId()}
+
+					<div
+						class:list-item-even={row.index % 2 === 0}
+						class:list-item-odd={row.index % 2 === 1}
+						style="position: absolute; top: 0; left: 0; width: 100%; height: {row.size}px; transform: translateY({row.start}px); padding-left: {$virtualizedTree
+							.visibleItems[row.index].level * 20}px;"
+					>
+						<button
+							style="display: flex; align-items: center; gap: 0.25rem; white-space: nowrap; padding: 0.25rem; background-color: {$virtualizedTree.getSelectedId() ===
+							id
+								? 'rgb(203 213 225);'
+								: ''};"
+							on:click={() => {
+								$virtualizedTree.toggleNode(id, row.index);
+							}}
+							aria-expanded={isExpanded}
+						>
+							<!-- Add icon. -->
+							{#if icon === 'plus' && hasChildren && isExpanded}
+								<svelte:component this={icons['chevron-down']} class="h-4 w-4" />
+							{:else if icon === 'plus' && hasChildren && !isExpanded}
+								<svelte:component this={icons['plus']} class="h-4 w-4 " />
+							{/if}
+							<span class="select-none">{content}</span>
+							<!-- Selected icon. -->
+							{#if selectedId === id}
+								<svelte:component this={icons['check']} class="h-4 w-4 " style="stroke:#22c55e" />
+							{/if}
+						</button>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
